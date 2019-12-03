112eb86636c375c05eea1859a94d62a347b10e3a
= <=>
2019-12-03 12:58:05 +0300

    minimize scripts and styles

--- js/anchors.js
+++ js/anchors.js
@@ -1,17 +1,2 @@
-$(document).ready(function () {
-    function anchorsFunc() {
-        const anchors = document.querySelectorAll('a[href*="#"]');
-        for (let anchor of anchors) {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const blockID = anchor.getAttribute('href');
-                document.querySelector(blockID).scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
-            });
-        }
-    }
-    anchorsFunc();
-
-});
+$(document).ready(function(){function anchorsFunc(){const anchors=document.querySelectorAll('a[href*="#"]');for(let anchor of anchors){anchor.addEventListener('click',function(e){e.preventDefault();const blockID=anchor.getAttribute('href');document.querySelector(blockID).scrollIntoView({behavior:'smooth',block:'start'})})}}
+anchorsFunc()})
