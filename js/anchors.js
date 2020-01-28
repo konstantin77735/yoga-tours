269bbd413c45413f332cc50eca53239dc1976209
Konstantin <51274871+konstantinkot@users.noreply.github.com>
2020-01-28 05:10:13 +0300

    Delete anchors.js

--- js/anchors.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){function anchorsFunc(){const anchors=document.querySelectorAll('a[href*="#"]');for(let anchor of anchors){anchor.addEventListener('click',function(e){e.preventDefault();const blockID=anchor.getAttribute('href');document.querySelector(blockID).scrollIntoView({behavior:'smooth',block:'start'})})}}
-anchorsFunc()})
